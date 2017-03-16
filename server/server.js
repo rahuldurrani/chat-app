1f08fedcc1c570a135b622b93c9780be59474d0b
Rahul Durrani <rdurrani@stevens.edu>
2017-03-16 16:11:41 -0400

    Add form for messages and show in browser

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const { generateMessage } = require('./utils/message');
 
+const { generateMessage } = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -13,19 +13,28 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log("New user connected");
+    console.log('New user connected');
 
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
-    socket.on('createMessage', (message) => {
-        console.log('Create message', message);
+
+    socket.on('createMessage', (message, callback) => {
+        console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
-        socket.on('disconnect', () => {
-            console.log("Client disconnected");
-        });
+        callback('This is from the server');
+        // socket.broadcast.emit('newMessage', {
+        //   from: message.from,
+        //   text: message.text,
+        //   createdAt: new Date().getTime()
+        // });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
     });
 });
 
 server.listen(port, () => {
-    console.log(`Started listening on port ${port}`);
+    console.log(`Server is up on ${port}`);
 });
