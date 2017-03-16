85054b39e21f88ae395336723eafaefd472aef5d
Rahul Durrani <rdurrani@stevens.edu>
2017-03-16 14:29:43 -0400

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,13 +15,28 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log("New user connected");
 
+    socket.emit('newMessage', {
+        from: 'admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage', {
+        from: 'admin',
+        text: 'New User joined the chat room',
+        createdAt: new Date().getTime()
+    });
     socket.on('createMessage', (message) => {
         console.log('Create message', message);
         io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        })
+                from: message.from,
+                text: message.text,
+                createdAt: new Date().getTime()
+            })
+            // socket.broadcast.emit('newMessage', {
+            //     from: message.from,
+            //     text: message.text,
+            //     createdAt: new Date().getTime()
+            // });
     });
     socket.on('disconnect', () => {
         console.log("Client disconnected");
