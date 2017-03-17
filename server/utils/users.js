26e6453e6a3feaf40e5e749fda724f08f18c758c
Rahul Durrani <rdurrani@stevens.edu>
2017-03-17 03:37:11 -0400

    add users class and update user list event

---
+++ server/utils/users.js
@@ -0,0 +1,45 @@
+// [{
+//     id: 'fre43534f3rf3425542',
+//     name: 'Rahul',
+//     room: 'Room A'
+// }]
+
+//addUser
+//removeUser(id)
+//getUser(id)
+//getUserList(room)
+
+class Users {
+    constructor() {
+        this.users = [];
+    }
+
+    addUser(id, name, room) {
+        var user = { id, name, room };
+        this.users.push(user);
+        return user;
+    }
+    removeUser(id) {
+        var user = this.getUser(id);
+        if (user) {
+            this.users = this.users.filter((user) => user.id !== id);
+        }
+        return user;
+    }
+
+    getUser(id) {
+        return this.users.filter((user) => user.id === id)[0];
+    }
+
+    getUserList(room) {
+        var users = this.users.filter((user) => {
+            return user.room === room
+        });
+
+        var namesArray = users.map((user) => user.name);
+
+        return namesArray;
+    }
+}
+
+module.exports = { Users };
