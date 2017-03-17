c06d3a99cb82de919e02f9b6d467e98a1efe06d8
Rahul Durrani <rdurrani@stevens.edu>
2017-03-16 22:00:02 -0400

    Added timestamps for messages

--- server/server.js
+++ server/server.js
@@ -22,17 +22,13 @@ io.on('connection', (socket) => {
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('This is from the server');
-        // socket.broadcast.emit('newMessage', {
-        //   from: message.from,
-        //   text: message.text,
-        //   createdAt: new Date().getTime()
-        // });
+        callback();
     });
 
     socket.on('createLocationMessage', (coords) => {
         io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
-    })
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
